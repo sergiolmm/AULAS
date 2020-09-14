75ed84631fcd8965eef93561ae8b6ab261b92fbe
Sergio Marques <sergio@unicamp.br>
2020-09-14 15:04:03 -0300

    Aula de geolocalizaçao

---
+++ geo_vesp/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:geo_vesp/pricipal.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: principal(),
+
+  ));
+}
