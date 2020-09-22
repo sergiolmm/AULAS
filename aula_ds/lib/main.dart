6e98db66572c00f542dc834d9fff29c23b8fa426
Sergio Marques <sergio@unicamp.br>
2020-09-22 14:20:21 -0300

    "aula_ds"

---
+++ aula_ds/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:aula_ds/MainScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainScreen(),
+  ));
+}
+
